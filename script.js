f55f5c047ac65ccc13b78772c8927dc69fd52251
celereigh <155793320+celereigh@users.noreply.github.com>
2026-03-13 22:04:16 -0400

    pickle rick murder egg

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+let slideIndexes = [
+    [1,1],
+    [1,1],
+];
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+
+
+function plusSlides(inc, item) {
+    console.log("IT WAS CKICEJD", inc, item)
+  let found = false
+  document.querySelectorAll('.'+item)
+  .forEach(slide => {
+    if (found) return
+    let slideIndex = slide.getAttribute("slide")
+    if(slide.style.display == "block"){
+        //this slide is active
+        found = true
+        slide.style.display = 'none'
+        let find = '.'+item+`[slide='${parseInt(slideIndex)+inc}']`
+        let element = document.querySelector(find)
+
+        if (!element) {
+            if (inc < 0) find = '.'+item+`[slide='${findMaxSlide(item)}']`
+            else find = '.'+item+`[slide='1']`
+            element = document.querySelector(find)
+        }
+
+        element.style.display='block' 
+    }
+  })
 }
 
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";  
+function findMaxSlide(classname){
+    let max = 0
+    document.querySelectorAll('.'+classname)
+    .forEach(slide => {
+        let slideIndex = slide.getAttribute("slide")
+        max = Math.max(max, parseInt(slideIndex))
+    })
+    return max
 }
+
